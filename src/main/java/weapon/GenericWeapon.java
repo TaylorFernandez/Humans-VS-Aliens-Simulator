f9c001d53e23899feba33a3c8a121f9751b12c3e
Yong Hang Lin <yl3139@engr.ship.edu>
2021-10-10 17:37:58 -0400

    genericWeapon

---
+++ src/main/java/weapon/GenericWeapon.java
@@ -0,0 +1,53 @@
+package weapon;
+
+public abstract class GenericWeapon extends java.lang.Object implements Weapon {
+  protected int baseDamage;
+  protected int currentAmmo;
+  protected int maxAmmo;
+  protected int maxRange;
+  protected int rateOfFire;
+  protected int shotsLeft;
+  
+  public GenericWeapon(){
+  }
+  
+  public abstract int fire(int distance);
+  
+  public abstract java.lang.String toString();
+  
+  public int getNumAttachments() {
+    return 0;
+  }
+  
+  public void reload() {
+    currentAmmo = maxAmmo;
+  }
+  
+  public int getBaseDamage() {
+    return baseDamage;
+  }
+  
+  public int getMaxRange() {
+    return maxRange;
+  }
+  
+  public int getRateOfFire() {
+    return rateOfFire;
+  }
+  
+  public int getMaxAmmo() {
+    return maxAmmo;
+  }
+  
+  public int getCurrentAmmo() {
+    return currentAmmo;
+  }
+  
+  public int getShotsLeft() {
+    return shotsLeft;
+  }
+  
+  public void updateTime(int time) {
+    
+  }
+}
