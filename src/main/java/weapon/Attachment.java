46e8cc0e4520a94a28993f13edd872c4cf0745b2
evan <ep7481@ship.edu>
2021-10-13 23:27:19 -0400

    added empty constructor in attachment and Finished Scope class

--- src/main/java/weapon/Attachment.java
+++ src/main/java/weapon/Attachment.java
@@ -1,80 +1,86 @@
 package weapon;
 
-public abstract class Attachment implements Weapon{
-    protected Weapon base; 
-    
-    /**
-     * Atachment constructor
-     * @param baseWeapon - weapon attachment is being added to
-     */
-    public Attachment(Weapon baseWeapon) {
-      base = baseWeapon;
-    }
-    
-    /**
-     * Fires the weapon. Accounts for changes to damage done by attachments
-     * @param distance - distance between weapon and target
-     */
-    public abstract int fire(int distance);
-    
-    public int getBaseDamage() {
-      return base.getBaseDamage();
-    }
-    
-    /**
-     * gets the current ammo of the weapon
-     */
-    public int getCurrentAmmo() {
-      return base.getCurrentAmmo();
-    }
-    
-    /**
-     * gets the maximum ammo the base weapon can hold
-     */
-    public int getMaxAmmo() {
-      return base.getMaxAmmo();
-    }
-    
-    /**
-     * Gets the maximum range the base weapon can shoot
-     */
-    public int getMaxRange() {
-      return base.getMaxRange();
-    }
-    
-    /**
-     * Gets the number of attachments the base weapon has
-     */
-    public int getNumAttachments() {
-      return base.getNumAttachments();
-    }
-    
-    /**
-     * Gets the rate of fire of the base weapon
-     */
-    public int getRateOfFire() {
-      return base.getRateOfFire();
-    }
-    
-    /**
-     * gets the shots the weapon can shoot in the current round
-     */
-    public int getShotsLeft() {
-      return base.getShotsLeft();
-    }
-    
-    /**
-     * reloads the weapon
-     */
-    public void reload() {
-      base.reload();
-    }
-    
-    /**
-     * notifies the weapon/attachments when a round has gone by
-     * @param time - current round
-     */
-    public void updateTime(int time) {
-      reload();
-    }
+public abstract class Attachment implements Weapon {
+		protected Weapon base;
+
+		public Attachment() {
+		}
+
+		/**
+		 * Atachment constructor
+		 * 
+		 * @param baseWeapon - weapon attachment is being added to
+		 */
+		public Attachment(Weapon baseWeapon) {
+				base = baseWeapon;
+		}
+
+		/**
+		 * Fires the weapon. Accounts for changes to damage done by attachments
+		 * 
+		 * @param distance - distance between weapon and target
+		 */
+		public abstract int fire(int distance);
+
+		public int getBaseDamage() {
+				return base.getBaseDamage();
+		}
+
+		/**
+		 * gets the current ammo of the weapon
+		 */
+		public int getCurrentAmmo() {
+				return base.getCurrentAmmo();
+		}
+
+		/**
+		 * gets the maximum ammo the base weapon can hold
+		 */
+		public int getMaxAmmo() {
+				return base.getMaxAmmo();
+		}
+
+		/**
+		 * Gets the maximum range the base weapon can shoot
+		 */
+		public int getMaxRange() {
+				return base.getMaxRange();
+		}
+
+		/**
+		 * Gets the number of attachments the base weapon has
+		 */
+		public int getNumAttachments() {
+				return base.getNumAttachments();
+		}
+
+		/**
+		 * Gets the rate of fire of the base weapon
+		 */
+		public int getRateOfFire() {
+				return base.getRateOfFire();
+		}
+
+		/**
+		 * gets the shots the weapon can shoot in the current round
+		 */
+		public int getShotsLeft() {
+				return base.getShotsLeft();
+		}
+
+		/**
+		 * reloads the weapon
+		 */
+		public void reload() {
+				base.reload();
+		}
+
+		/**
+		 * notifies the weapon/attachments when a round has gone by
+		 * 
+		 * @param time - current round
+		 */
+		public void updateTime(int time) {
+				base.reload();
+		}
 }
