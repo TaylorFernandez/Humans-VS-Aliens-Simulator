30b17c7a68c7a443b96546a4a61b697fc83d2eca
taylorfernandez <taylorfernandez@localhost>
2021-11-14 18:25:37 -0500

    Implemented drop command

--- src/main/java/commands/DropCommand.java
+++ src/main/java/commands/DropCommand.java
@@ -1,5 +1,25 @@
 package commands;
 
-public class DropCommand {
+import environment.Environment;
+import gameUI.GameUI;
+import gameUI.gameCell;
+import lifeform.LifeForm;
 
+public class DropCommand implements Commands{
+  protected Environment environment;
+  protected GameUI  ui;
+    public DropCommand(Environment env, GameUI ui) {
+      environment = env;
+      this.ui = ui;
+    }
+        
+    public void execute() {
+      gameCell cell = ui.getHighlighted();
+      
+      LifeForm form = environment.getCell(cell.getRow(), cell.getCol()).getLifeForm();
+      
+      form.dropWeapon();
+      
+      ui.drawCell(cell.getRow(), cell.getCol(), environment, cell);
+    }
 }
---
+++ src/main/java/gameUI/gameCell.java
@@ -0,0 +1,55 @@
+package gameUI;
+
+import javax.swing.JButton;
+
+public class gameCell extends JButton {
+
+  private static final long serialVersionUID = 1L;
+  public int row;
+  public int col;
+  public boolean isHighlighted = false;
+  public gameCell(int r, int c) {
+    super();
+    row = r;
+    col = c;
+  }
+  /**
+   * gets the row a button is in
+   * @return row - row of the button
+   */
+  public int getRow() {
+    return row;
+  }
+  
+  /**
+   * gets the column a button is in
+   * @return col - column of the button
+   */
+  public int getCol() {
+    return col;
+  }
+  
+  /**
+   * sets the row of a button
+   * @param r - row of the button
+   */
+  public void setRow(int r) {
+    row = r;
+  }
+  
+  /**
+   * sets the column a button is in
+   * @param c - column of the button
+   */
+  public void setcol(int c) {
+    col = c;
+  }
+  
+  public boolean isHighlighted() {
+    return isHighlighted;
+  }
+  
+  public void setHighlighted(boolean setBool) {
+    isHighlighted = setBool;
+  }
+ }
