4462debb0d6875bca466ac8690dbfcddf6b5e805
socce <tp0227@engr.ship.edu>
2021-11-14 19:42:42 -0500

    fixed moveCommand

--- src/main/java/commands/MoveCommand.java
+++ src/main/java/commands/MoveCommand.java
@@ -2,21 +2,23 @@ package commands;
 
 import environment.Environment;
 
-public class MoveCommand implements Commands{
-  private Environment environment;
+public class MoveCommand implements Commands {
+	private Environment environment;
 
-  public MoveCommand(Environment e) {
-    environment = e;
-  }
-  
-  @Override
-  public void execute() {  
-    int row = environment.getSelectedRow();
-    int col = environment.getSelectedCol();
-    if (environment.getLifeForm(row, col) != null) {
-      environment.move(row, col);
-      
-      System.out.println("Execute north");
-    }
-  } 
+	public MoveCommand(Environment e) {
+		environment = e;
+	}
+
+	@Override
+	public void execute() {
+		int row = environment.getSelectedRow();
+		int col = environment.getSelectedCol();
+		if (environment.getLifeForm(row, col) != null) {
+			int x = environment.getCell(row, col).getLifeForm().getDirection();
+			System.out.println("Move executed " + x);
+			environment.move(row, col);
+			
+		}
+	}
+
 }
